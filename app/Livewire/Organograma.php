a47d2199d87da1ce959ef2deebf814a1f37c4acd
arthur-bpv <arthur.ambrosio2008@gmail.com>
2026-06-14 19:38:09 +0000

    Specify layout explicitly in Organograma component

--- app/Livewire/Organograma.php
+++ app/Livewire/Organograma.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('layouts.app')]
 class Organograma extends Component
 {
     public array $tree = [

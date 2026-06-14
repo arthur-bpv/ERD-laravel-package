363cf3262aeab7a6257e880d014eaf195ea33388
arthur-bpv <arthur.ambrosio2008@gmail.com>
2026-06-14 20:05:38 +0000

    Add Tailwind CDN as diagnostic test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <title>{{ $title ?? config('app.name') }}</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
 
         @livewireStyles
     </head>

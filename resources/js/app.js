90cc75c71005759a317d10c783c0f1449c702c10
arthur-bpv <arthur.ambrosio2008@gmail.com>
2026-06-14 21:36:54 +0000

    Fix AlpineFlow init: use Livewire ESM manual bundle to register plugin before Alpine starts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import AlpineFlow from '../../vendor/getartisanflow/wireflow/dist/alpineflow.bundle.esm.js';
 import './bootstrap';
 
-document.addEventListener('alpine:init', () => {
-    window.Alpine.plugin(AlpineFlow);
-});
+Alpine.plugin(AlpineFlow);
+window.Alpine = Alpine;
+
+Livewire.start();
